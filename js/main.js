097bfa2b660f79b25da27bdba5a0387d54b6de96
Joaquin Bustos <joaquinnicolasbustos@hotmail.com>
2026-03-19 13:38:49 -0300

    feat: se agrego todo el codigo js, haciendo que funcione el Tamaño de la paleta

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
+let btn = document.querySelectorAll(".buttons-group");
+let oculto = document.querySelectorAll(".card-extra");
+let card = document.querySelectorAll(".card");
 
-
-const boton = document.getElementById("botonInteractivo");
-
-boton.addEventListener("click", function (event) {
-  event.target.style.backgroundColor = "purple";
-  event.target.style.color = "white";
-  event.target.textContent = "Color cambiado!";
+btn.forEach((valor) => {
+  valor.addEventListener("click", () => {
+    if (valor.id === "btn-6") {
+      recorrerBtn(0);
+    }
+    if (valor.id === "btn-8") {
+      recorrerBtn(2);
+    }
+    if (valor.id === "btn-9") {
+      recorrerBtn(3);
+    }
+  });
 });
+
+function recorrerBtn(cantidad) {
+  oculto.forEach((valor, index) => {
+    valor.classList.add("oculto");
+
+    if (index < cantidad) {
+      valor.classList.remove("oculto");
+    }
+  });
+}
